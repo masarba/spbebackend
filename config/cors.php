142ef5bc9191b72a15bc4069c6488a6aa496e044
putra304 <ariyoputra04@gmail.com>
2025-07-13 13:57:52 +0700

    fix allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://spbeauditee.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
